6d3940f4ffab8eae83f8b964017454ca2c4eb77f
Lin <46348338+Heistoo@users.noreply.github.com>
2025-03-29 15:42:29 -0300

    :sparkles: add: integration with getlinks function

--- app/webscraping.py
+++ app/webscraping.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from utils.functions import ChooseLinks, GetRef
+from utils.functions import ChooseLinks, GetRef, DownloadContent, GenerateZip, GetChosenLinks
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 html = urlopen(url).read()
@@ -8,16 +8,24 @@ soup = BeautifulSoup(html, features="html.parser")
 
 # Chamando as funções do arquivo utils/functions.py
 # ChooseLinks(soup)
-verify = False
-while not verify:
-    try:
-        count = int(input("Quantos links você quer baixar? "))
-        if count > 0:
-            print("\nEscolha os links pelo texto:")
-            for _ in range(count):
-                GetRef(soup)
-            verify = True
-        else:
-            print("Digite um número maior que 0")
-    except ValueError:
-        print("Por favor, digite um número válido.")
+
+try:
+    count = int(input("\nQuantos links você quer escolher? "))
+    if count > 0:
+        # Obter os links escolhidos pelo usuário
+        chosen_links = GetChosenLinks(soup, count)
+
+        # Baixar os arquivos
+        downloaded_files = []  # Lista para armazenar os arquivos baixados
+        for i, link in enumerate(chosen_links, 1):
+            file_path = DownloadContent(link, f"downloaded_file_{i}")
+            if file_path:
+                downloaded_files.append(file_path)
+
+        # Compactar todos os arquivos baixados em um arquivo ZIP
+        if downloaded_files:
+            GenerateZip(downloaded_files, "downloads.zip")
+    else:
+        print("Digite um número maior que 0")
+except ValueError:
+    print("Por favor, digite um número válido.")
