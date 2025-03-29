114c903f5ee8dd63c3935f498a42d0c616feeb82
Lin <46348338+Heistoo@users.noreply.github.com>
2025-03-29 15:24:58 -0300

    :docs: fix: improve documentation

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 # function to find all links in a webpage and print them
 def ChooseLinks(soup):
-    """Exibe todos os links encontrados na página."""
+    """Exibe todos os links encontrados na página fornecida."""
     links = soup.find_all("a", href=True)
     for link in links:
         href = link['href']
@@ -11,14 +11,15 @@ def ChooseLinks(soup):
 
 # function to format link ensuring it has the full domain
 def FormatLink(href):
-    """Formata o link para garantir que tenha o domínio completo."""
+    """Formata o link para garantir que tenha um domínio completo."""
     if not href.startswith("http"):
         href = "https://www.gov.br" + href
     return href
 
 # function to get link based on user input
 def FindLink(links, choice):
-    """Busca o link pelo texto fornecido."""
+    """Busca o link pelo texto fornecido, passe o parâmetro que mostra todos os links seguido do texto que se assemelha ao 
+    que está no conteúdo do HTML da página."""
     for link in links:
         text = link.get_text().strip()
         if choice.lower() in text.lower():
@@ -27,7 +28,7 @@ def FindLink(links, choice):
 
 # function to get link href and return it for webscraping unit testing
 def GetRef(soup):
-    """Permite escolher um link pelo texto e retorna o link completo."""
+    """Permite escolher um link pelo texto e retorna o link completo. Passe o parâmetro que manipula o HTML"""
     links = soup.find_all("a", href=True) # get all links from page 
     while True:
         choice = input("Digite o texto do anexo que você quer baixar: ").strip()
